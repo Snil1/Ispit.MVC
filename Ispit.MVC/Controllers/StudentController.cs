063e2fc78f235c601afa7a5f99ea7477683e10e0
Stipe Vuletin <stipevuletin@gmail.com>
2023-05-15 21:39:48 +0200

    Add project files.

---
+++ Ispit.MVC/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using Ispit.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ispit.MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        private List<Student> studenti = new List<Student>()
+        {
+            new Student(){Id = 1, Name = "Ana Anić", Age = 32, Bio = "FSB" },
+            new Student(){Id = 2, Name = "Ivo Ivić", Age = 35, Bio = "FER"},
+            new Student(){Id = 3, Name = "Mate Matić", Age = 20, Bio = "Filozofski fakultet"},
+            new Student(){Id = 4, Name = "Iva Ivić", Age = 22, Bio = "Ekonomski fakultet"},
+            new Student(){Id = 5, Name = "Ante Antić", Age = 25, Bio = "FESB"}
+        };
+
+
+        public IActionResult Index()
+        {
+
+
+            return View(studenti);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Student student = studenti.FirstOrDefault(s=>s.Id == id);
+            return View(student);
+        }
+    } 
+}
---
+++ Ispit.MVC/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace Ispit.MVC.Models
+{
+    public class Student
+    {
+
+        [DisplayName("Šifra studenta:")]
+        public int Id { get; set; }
+
+        [DisplayName("Ime i prezime:")]
+        public string Name { get; set; }
+
+        [DisplayName("Starost:")]
+        public int Age { get; set; }
+
+        [DisplayName("Fakultet:")]
+        public string Bio { get; set; }
+    }
+}
